8fc5ea0a5a30d7a575009b3b2301bb948be31139
Tânagra Andria <design.andria@gmail.com>
2020-10-28 14:53:31 -0300

    teste

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,20 @@
+ /* CIFRA DE CESAR
+A   B  C  D  E  F  G  H  I  J  K   L   
+1  2  3  4  5  6  7  8  9  10  11  12
+65  66 67 68 69 70 71 72 73 74 75  76  
+
+M   N   O   P   Q   R   S    T  U   V   W   X   Y   Z
+13  14  15  16  17  18  19  20  21  22  23  24  25  26
+77  78  79  80  81  82  83  84  85  86  87  88  89  90
+*/
+// js puro aqui o texto vai ser criptografado e descriptografado
 const cipher = {
-  // ...
+  /*(codigo da letra + deslocamento) % tamanaho do alfabeto*/ ...
+codigoASC=> codigo0a25 => desloco=> giro => codigoASC
+  ((codigoDaLetraASC - codigoDaLetra + chaveDeDeslocamento) %26) + codigoDaLetra
+function CIFRA(String){
+  return
+}
 };
 
 export default cipher;
