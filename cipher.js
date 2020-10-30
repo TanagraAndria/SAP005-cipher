19e8fe3d5eb7a9353d930e74a9484295028d7653
Tânagra Andria <design.andria@gmail.com>
2020-10-30 15:25:55 -0300

    add imagem, botões, caixa de mensagem

--- cipher.js
+++ cipher.js
@@ -1,20 +1,24 @@
  /* CIFRA DE CESAR
 A   B  C  D  E  F  G  H  I  J  K   L   
-1  2  3  4  5  6  7  8  9  10  11  12
+0   1  2  3  4  5  6  7  8  9  10  11 
 65  66 67 68 69 70 71 72 73 74 75  76  
 
 M   N   O   P   Q   R   S    T  U   V   W   X   Y   Z
-13  14  15  16  17  18  19  20  21  22  23  24  25  26
+12  13  14  15  16  17  18  19  20  21  22  23  24  25  (26 letras) 
 77  78  79  80  81  82  83  84  85  86  87  88  89  90
 */
+//   /*(codigo da letra + deslocamento) % tamanaho do alfabeto*/ ...
+// codigoASC=> codigo1a25 => desloco=> giro => codigoASC
 // js puro aqui o texto vai ser criptografado e descriptografado
-const cipher = {
-  /*(codigo da letra + deslocamento) % tamanaho do alfabeto*/ ...
-codigoASC=> codigo0a25 => desloco=> giro => codigoASC
-  ((codigoDaLetraASC - codigoDaLetra + chaveDeDeslocamento) %26) + codigoDaLetra
-function CIFRA(String){
-  return
-}
-};
 
-export default cipher;
+codigoASC=> codigo0a25 
+
+ const cipher = {
+
+((codigoDaLetraASC - codigoDaLetra + chaveDeDeslocamento) %26) + codigoDaLetra
+// function CIFRA(String){
+//   return
+// }
+ };
+
+ export default cipher;
--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+//.submit(function()
+// console.log "oi" e "thau" para testar o botão
+
 import cipher from './cipher.js';
+
+const botaocriptografar= document.getElementById("botaocriptografar")
+console.log(botaocriptografar);
+botaocriptografar.addEventListener("click",function name(event) {
+    event.preventDefault()
+    console.log("oi")    
+})
+
+const botaodescriptografar= document.getElementById("botaodescriptografar")
+console.log(botaodescriptografar);
+botaodescriptografar.addEventListener("click",function name(event) {
+    event.preventDefault()
+    console.log("thau")    
+})
+
+
+
 //Qual o texto, quantas casas é para andar
+
+
 console.log(cipher);
