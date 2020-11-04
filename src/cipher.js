7305743020a40c8261e493ad495144b87ad341db
Tânagra Andria <design.andria@gmail.com>
2020-11-04 18:54:53 -0300

    ajustes gerais

--- src/cipher.js
+++ src/cipher.js
@@ -7,26 +7,44 @@ M   N   O   P   Q   R   S    T  U   V   W   X   Y   Z
 12  13  14  15  16  17  18  19  20  21  22  23  24  25  (26 letras) 
 77  78  79  80  81  82  83  84  85  86  87  88  89  90
 */
-//   /*(codigo da letra + deslocamento) % tamanaho do alfabeto*/ ...
+//  (codigo da letra + deslocamento) % tamanaho do alfabeto ...
 // codigoASC=> codigo1a25 => desloco=> giro => codigoASC
-// js puro aqui o texto vai ser criptografado e descriptografado
- 
+// js puro aqui o texto vai ser criptografado e descriptografado 
 //codigoASC= codigo0a25 
 //códigodaletra= 65
- 
+ // return = mensagemCodificada; 
 const cipher = {
-  encode: function (string){ 
-    for (let i = 0; i < caixademensagem.lenght; i++ )
-    mensagemCodificada= ((codigoASC- 65 + offiset) %26 +65);
-   // return = mensagemCodificada;
-    function codigoASC (a) { return a.charCodeAt(0) }
-},
+ // encode: function (offiset, string) { 
+
+  //  if (offiset ==""|| string ==""|| typeof offiset == "number" || typeof string == "string"){
+  //    throw new TypeError ("Digite sem números")
+  //  } 
+  //  else {
+   //    function encode (offiset,string) {
+   //     if (offiset==""|| string==""|| typeofypeof offiset == "number" || typeof string == "string"){
+  //        throw new TypeError ("Digite sem números")
+  //      }
+  //}
+
+ //   let mensagemCodificada= "";  
+  //  for (let i = 0; i < caixademensagem.lenght; i++ ) {
+  //   let codigoASC = string.charCodeAt(i);
+  //   let forEncode = ((codigoASC- 65 + offiset) %26 + 65);
+  //  mensagemCodificada += String.fromCharCode(forEncode);
+  //  } return mensagemCodificada;
+  
+   // }
+ // },
 
 
- decode: function ( ) {
-
-  }
-
+  //decode: function (offiset, string ) {
+  //let  mensagemCodificada= ""; 
+  //for (let i = 0; i < caixademensagem.lenght; i++ ){
+  // let codigoASC = string.charCodeAt(i); 
+  // let forDecode= ((codigoASC+ 65 - offiset) %26 -65);
+  // mensagemCodificada += String.fromCharCode(forDecode);
+ // } return  mensagemCodificada;
+ // }
   // ...
 };
 
--- src/index.js
+++ src/index.js
@@ -6,27 +6,24 @@
 //}
 //document.getElementById("caixademensagem").innerHTML = text;
 //Qual o texto, quantas casas é para andar
+
 import cipher from './cipher.js';
 
 const botaocriptografar= document.getElementById("botaocriptografar");
-botaocriptografar.addEventListener("click",  cipher);
-
-function criptografar (event){
- event.preventDefault();
-let offiset = Number (document.getElementById("offiset").value);
-console.log (typeof offiset)
-let caixademensagem= document.getElementById ("caixademensagem").value.toUpperCase();        
-let mensagem;
-document.getElementById("caixademensagem").innerHTML=mensagemCodificada;
-let mensagemCodificada;
-}
-
+botaocriptografar.addEventListener("click",  crip)
 
 const botaodescriptografar= document.getElementById("botaodescriptografar");
-botaodescriptografar.addEventListener("click", decipher); 
+botaodescriptografar.addEventListener("click",crip); 
 
-function decipher () {
-
+function crip (event){
+    event.preventDefault();   
+    let offiset = Number(document.getElementById("offiset").value);
+    let caixademensagem= document.getElementById ("caixademensagem").value.toUpperCase();        
+    let mensagemCodificada= "";
+    mensagemCodificada;
+    caixademensagem;
+    document.getElementById("caixademensagem").innerHTML= mensagemCodificada;
+    console.log (typeof offiset)      
 }
 
 console.log(cipher);
